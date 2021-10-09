3bc962d4209f41dbb55627fb7333d54247b285cd
ryuju1029 <ryuju19971029@gmail.com>
2021-10-09 22:46:28 +0900

    ログアウト、headerのレイアウト調整

---
+++ header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style.css">
+<header>
+  <div class="headerContent">
+    <a class="headerContentList">
+      <font color="#FFFFFF">タスク追加
+    </a>
+    <a class="headerContentList">
+      <font color="#FFFFFF">カテゴリ一覧
+    </a>
+    <a class="headerContentList" href="logout.php">
+      <font color="#FFFFFF">ログアウト
+    </a>
+  </div>
+</header>
