36954864ea731a13eb73bccb8b7ccadfb2167d7d
ryuju1029 <ryuju19971029@gmail.com>
2021-10-21 14:42:05 +0900

    検索機能実装

--- Dao/TaskDao.php
+++ Dao/TaskDao.php
@@ -119,4 +119,58 @@ EOF;
       $task['updated_at']
     );
   }
+
+  public function SearchByTask(int $status, int $user_id, string $contents)
+  {
+    $sql = <<<EOF
+     SELECT 
+      t.id,
+      t.user_id,
+      t.contents,
+      t.status,
+      t.deadline,
+      t.created_at,
+      t.updated_at,
+      c.id as category_id,
+      c.name as category_name 
+     FROM 
+      tasks t 
+     JOIN 
+      categories c 
+     ON 
+      t.category_id = c.id 
+     WHERE 
+      t.user_id = :user_id 
+     AND 
+      t.status = :status 
+     AND 
+      t.contents 
+     LIKE 
+      :contents
+    EOF;
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindValue(':status', $status);
+    $stmt->bindValue(':user_id', $user_id);
+    $stmt->bindValue(':contents', $contents);
+    $stmt->execute();
+    $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($tasks === false) return [];
+
+    $taskRows = [];
+    foreach ($tasks as $task) {
+      $taskRows[] = new TaskJoinCategoryRaw(
+        $task['id'],
+        $task['user_id'],
+        $task['status'],
+        $task['contents'],
+        $task['category_id'],
+        $task['deadline'],
+        $task['created_at'],
+        $task['updated_at'],
+        $task['category_name']
+      );
+    }
+    return $taskRows;
+  }
 }
