e70f148435e13909e1914261129e7c1d2ecfcc1d
ryuju1029 <ryuju19971029@gmail.com>
2021-11-22 13:31:40 +0900

    タスク完了未完了機能をリポジトリを経由　して実装

--- updateStatus.php
+++ updateStatus.php
@@ -1,9 +1,12 @@
 <?php
-require_once(__DIR__ . '/Dao/TaskDao.php');
+require_once(__DIR__ . '/Repository/TaskRepository.php');
 require_once(__DIR__ . '/Lib/Redirect.php');
 $id = filter_input(INPUT_GET, "id");
-$taskDao = new TaskDao();
-$task = $taskDao->findById($id);
-$status = $task->status() === 0 ? 1 : 0;
-$task = $taskDao->updateStatus($id, $status);
+$taskRepository = new TaskRepository();
+$taskId = new TaskId($id);
+$task = $taskRepository->findById($taskId);
+$status = $task->status()->value() === 0 ? 1 : 0;
+$taskStatus = new TaskStatus($status);
+$taskRepository->updateStatus($taskId, $taskStatus);
+
 Redirect::handler('/ToDo/index.php');
